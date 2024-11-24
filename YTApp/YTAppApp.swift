a250f8f4d06a32f4d98e8246b2ba8e0e94851e18
shepel04 <95418153+shepel04@users.noreply.github.com>
2024-11-24 10:02:06 +0200

    Initial Commit

---
+++ YTApp/YTAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YTAppApp.swift
+//  YTApp
+//
+//  Created by macbook on 24.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct YTAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
